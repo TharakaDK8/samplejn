2cf6725d1f81b3c778d047801f141a9d4db61225
Tharaka Dayanjana <47805548+TharakaDK8@users.noreply.github.com>
2024-06-27 11:52:25 +0530

    add file

---
+++ Test.java
@@ -0,0 +1,23 @@
+public class Test {
+    public static void main(String[] args) {
+        // Display a welcome message
+        System.out.println("Welcome to the Test Program!");
+
+        // Perform basic arithmetic operations
+        int a = 5;
+        int b = 3;
+
+        int sum = a + b;
+        int difference = a - b;
+        int product = a * b;
+        int quotient = a / b;
+        int remainder = a % b;
+
+        // Print the results
+        System.out.println("The sum of " + a + " and " + b + " is: " + sum);
+        System.out.println("The difference between " + a + " and " + b + " is: " + difference);
+        System.out.println("The product of " + a + " and " + b + " is: " + product);
+        System.out.println("The quotient of " + a + " divided by " + b + " is: " + quotient);
+        System.out.println("The remainder of " + a + " divided by " + b + " is: " + remainder);
+    }
+}
